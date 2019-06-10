43a46472ceaca807a431f42d145544c48fec087c
Daan Wilson <daan@lined.nl>
2019-06-10 21:11:43 +0200

    Composer update added some files

---
+++ vendor/bunq/sdk_php/src/Model/Generated/Endpoint/BunqResponseRewardSender.php
@@ -0,0 +1,17 @@
+<?php
+namespace bunq\Model\Generated\Endpoint;
+
+use bunq\Http\BunqResponse;
+
+/**
+ */
+class BunqResponseRewardSender extends BunqResponse
+{
+    /**
+     * @return RewardSender
+     */
+    public function getValue(): RewardSender
+    {
+        return parent::getValue();
+    }
+}
